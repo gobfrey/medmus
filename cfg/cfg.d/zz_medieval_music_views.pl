1840c9a670bc5708a022964b9a44141f20ba9de3
Adam Field <adam@adamfield.net>
2013-05-29 10:52:00 +0100

    browse view setup

---
+++ cfg/cfg.d/zz_medieval_music_views.pl
@@ -0,0 +1,66 @@
+
+# Browse views. allow_null indicates that no value set is 
+# a valid result. 
+# Multiple fields may be specified for one view, but avoid
+# subject or allowing null in this case.
+$c->{browse_views} = [
+        {
+                id => "type",
+                menus => [
+			{
+				fields => ["medmus_type"],
+			}
+		],
+                order => "eprintid",
+        },
+];
+
+# examples of some other useful views you might want to add
+#
+# Browse by the ID's of creators & editors (CV Pages). Useful to import the 
+# .include part into your main website or their homepage, rather than access
+# directly via the eprints website.
+#        {
+#                id => "person",
+#                menus => [
+#			{
+#				fields => [ "creators_id","editors_id" ],
+#                		allow_null => 0,
+#			}
+#		],
+#                order => "-date/title",
+#                noindex => 1,
+#                nolink => 1,
+#                nocount => 0,
+#                include => 1,
+#        },
+
+# Browse by the names of creators (less reliable than Id's), section the menu 
+# by the first 3 characters of the surname, and if there are more than 30 
+# names, split the menu up into sub-pages of around 30.
+# Show the list of names in 3 columns.
+#
+#
+#	{ 
+#		id => "people", 
+#		menus => [ 
+#			{ 
+#				fields => ["creators_name","editors_name"], 
+#				allow_null => 0,
+#				grouping_function => "EPrints::Update::Views::group_by_3_characters",
+#				group_range_function => "EPrints::Update::Views::cluster_ranges_30",
+#				mode => "sections",
+#				open_first_section => 1,
+#				new_column_at => [0,0],
+#			} 
+#		],
+#		order=>"title",
+#	},
+
+
+# Browse by the type of eprint (poster, report etc).
+#{ id=>"type", menus=>[ { fields=>"type" } ], order=>"-date" }
+
+
+
+
